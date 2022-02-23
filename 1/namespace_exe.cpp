673a189523c8e2d9baecfe86579f905b6268db2b
Tal <talzhtc@gmail.com>
2022-02-23 02:48:46 +0200

    new file

---
+++ 1/namespace_exe.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main(){
+    // Create two different namespaces and in each one write a function to print the name
+    // of the namespace. Name the BOTH functions 'foo'
+
+    // Try to call the functions:
+    // 1. Without using statements
+    // 2. With a single using statement
+    // 3. With two using statements
+    // Write down comments on what happened each time. What did you understand?
+    
+    // Play with it!
+
+    return 0;
+}
