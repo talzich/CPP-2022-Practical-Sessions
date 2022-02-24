9a83f44e9d06eb689ce7de3cdd3e1732edf41c6f
Tal <talzhtc@gmail.com>
2022-02-24 20:36:30 +0200

    new file

---
+++ 2-classes/overload.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    // write a function named 'add' that sums two integers
+    // write a method named 'add' that sums two real numbers
+    // write a function, also named 'add' that concatenates two strings
+
+    // 1. Call 'add' with two integers
+    // 2. Call 'add' with two strings
+    // 3. Call 'add' with two doubles
+    // 4. try one of each
+
+    // Explain, in your own words in a comment, what do you think the difference is
+    // between 'ambiguous' and 'no matching function'
+    return 0;
+}
