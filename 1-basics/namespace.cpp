5466246e4fc5cc5118199aae69788d5559117d36
Tal <talzhtc@gmail.com>
2022-02-26 23:56:58 +0200

    fixed grammer issues

--- 1-basics/namespace.cpp
+++ 1-basics/namespace.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
-
 int main(){
     // Create two different namespaces and in each one write a function to print the name
-    // of the namespace. Name the BOTH functions 'foo'
+    // of the namespace. Name BOTH functions 'foo'
 
     // Try to call the functions:
-    // 1. Without using statements
-    // 2. With a single using statement
-    // 3. With two using statements
+    // 1. Without 'using' statements
+    // 2. With a single 'using' statement
+    // 3. With two 'using' statements
     // Write down comments on what happened each time. What did you understand?
     
     // Play with it!
